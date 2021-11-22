7a8777e1fa4d4fcbbdb600364990f2a3d62c72b1
Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2021-11-22 17:07:13 +0700

    Create ubuntu-20.04-install-wrk-load-testing.sh

---
+++ ubuntu-install/ubuntu-20.04-install-wrk-load-testing.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Build
+sudo apt-get install build-essential libssl-dev git -y
+
+# Setup
+sudo -i
+mkdir -p /studio/build
+
+# Install Wrk Load-Testing
+cd /studio/build && git clone https://github.com/wg/wrk.git wrk
+cd /studio/build/wrk && sudo make
+
+sudo cp wrk /usr/local/bin
+wrk --version
